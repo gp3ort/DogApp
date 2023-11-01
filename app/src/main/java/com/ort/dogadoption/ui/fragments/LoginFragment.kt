40218718e779ad0919f54d97dbb107a98567911f
Lucas Martin Funes <38304769@comunidadort.edu.ar>
2023-11-01 11:10:52 -0300

    Login functionality, finished!

--- app/src/main/java/com/ort/dogadoption/ui/fragments/LoginFragment.kt
+++ app/src/main/java/com/ort/dogadoption/ui/fragments/LoginFragment.kt
@@ -1,16 +1,25 @@
 package com.ort.dogadoption.ui.fragments
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.navigation.findNavController
 import com.ort.dogadoption.R
+import com.ort.dogadoption.ui.views.MainActivity
 
 
 class LoginFragment : Fragment() {
 
     lateinit var v: View
+    lateinit var loginButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +32,48 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         v = inflater.inflate(R.layout.fragment_login, container, false)
+
+        val usernameInput: TextView = v.findViewById(R.id.emailInputId)
+        val passwordInput: TextView = v.findViewById(R.id.passwordInputId)
+
+        setNavGraphButton(usernameInput, passwordInput)
+
         return v;
+    }
+
+    private fun setNavGraphButton(usernmameInput: TextView, passwordInput: TextView) {
+        val loginButton = v.findViewById<Button>(R.id.loginButtonId)
+
+        loginButton.setOnClickListener{
+
+            val email: String = usernmameInput.text.toString()
+            val password: String = passwordInput.text.toString()
+
+            if(validateInputData(email, password)){
+                val intent = Intent(activity, MainActivity::class.java)
+                intent.putExtra("user", email)
+                startActivity(intent)
+                activity?.finish()
+            }
+        }
+    }
+
+    private fun validateInputData(username: String, password: String): Boolean{
+        if (username.isEmpty()) {
+            displayToast("Ingrese usuario, por favor!")
+            return false
+        }
+
+        if (password.isEmpty()) {
+            displayToast("Ingrese una contraseña, por favor!")
+            return false
+        }
+
+        return true
+    }
+
+    private fun displayToast(message: String) {
+        Toast.makeText(activity, message, Toast.LENGTH_SHORT).show()
     }
 
 }
--- app/src/main/java/com/ort/dogadoption/ui/views/MainActivity.kt
+++ app/src/main/java/com/ort/dogadoption/ui/views/MainActivity.kt
@@ -2,11 +2,22 @@ package com.ort.dogadoption.ui.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import com.ort.dogadoption.R
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var textTest: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val email = intent.getStringExtra("user")
+
+        textTest = findViewById(R.id.textTestId)
+        textTest.text = email
+
     }
 }
